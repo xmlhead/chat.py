86ad6b3fe34dc482b259a483bd04839276aca243
Jörn Donges <103573423+xmlhead@users.noreply.github.com>
2024-08-09 17:48:49 +0200

    Update helpstring

--- chat.py
+++ chat.py
@@ -35,22 +35,10 @@ def process_response(response):
 
 helpstring = """
 
-Very simple command line client for OpenAI API LLMs (ChatGPT 4)
+Very simple command line client for OpenAI API LLMs (like ChatGPT-4)
+See https://github.com/xmlhead/chat.py for more info.
 
-Default config expected in a file called chat_config.json, example:
-```
-{ "url":"https://endpoint.com/chat/completions",
-"api_key":"ENV_OPENAI_API_KEY",
-"model": "anthropic-claude-3-5-sonnet",
-"available_models": [
-    "anthropic-claude-3-5-sonnet", 
-    "anthropic-claude-3-haiku", 
-    "llama3-70b", 
-    "mistral-large"
-    ],
-"role":"user",
-"temperature":"0.5" }
-```
+
 User Commands:
  - !exit:      Exit chat
  - !debug:     Print last response json from LLM
@@ -62,8 +50,7 @@ User Commands:
  - !save_config <filename>:    Save current config 
  - !help:      Print this text
 
-Note: API key can be set directly but it is recommended to keep the setting 
-and provie an environment variable called  OPENAI_API_KEY for security reasons. 
+ 
 """
 
 def load_config(configfilename):
